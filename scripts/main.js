76fb84cf4a2d41e1520a308f021abec2b468efe6
Richa.Christian <richa.christian@actonate.com>
2019-01-07 12:42:11 +0530

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png'){
+        myImage.setAttribute('src','images/chrome.jpeg');
+    }
+    else{
+        myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName(){
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name',myName);
+    myHeading.textContent = 'Welcome to Mozilla,' + myName;
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}
+else{
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome to Mozilla,' + storedName;
+}
+
+myButton.onclick = function(){
+    setUserName();
+}
